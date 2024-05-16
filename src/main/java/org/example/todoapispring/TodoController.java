a03769102e65124362e593dcf4e877362596f059
Himanshu Negi <himanshu.negi@freshworks.com>
2024-05-16 23:29:02 +0530

    Spring Rest Api

---
+++ src/main/java/org/example/todoapispring/TodoController.java
@@ -0,0 +1,32 @@
+package org.example.todoapispring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController // helps in object creation and
+public class TodoController {
+
+    public static List<Todo> todoList;
+
+    public TodoController() {
+        todoList = new ArrayList<>();
+        todoList.add(new Todo(1, "Todos1", false, 1));
+        todoList.add(new Todo(2, "Todos1", false, 2));
+    }
+
+    @GetMapping("/todos")
+    public List<Todo> getTodos() {
+        return todoList;
+    }
+
+    @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo newTodo) {
+        todoList.add(newTodo);
+        return newTodo;
+    }
+}
